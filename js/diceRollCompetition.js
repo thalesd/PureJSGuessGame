5568bfc315c110da80a4c0b92cecab2f0fd00b3b
Thales Donizetti <thales.doni.ol@gmail.com>
2021-11-10 11:23:24 -0300

    Finaliza exercicio 2

---
+++ js/diceRollCompetition.js
@@ -0,0 +1,49 @@
+var amigo1Rolou = false;
+var amigo2Rolou = false;
+
+var numerosAmigo1 = [];
+var numerosAmigo2 = [];
+
+function rolarDadosAmigo(numeroAmigo) {
+    document.getElementById('botaoAmigo-' + numeroAmigo).setAttribute('enabled', false);
+
+    if (numeroAmigo == 1) {
+        for (i = 0; i < 6; i++) {
+            numerosAmigo1.push(rolarDado());
+        }
+    } else {
+        for (i = 0; i < 6; i++) {
+            numerosAmigo2.push(rolarDado());
+        }
+    }
+
+    if (amigo1Rolou && amigo2Rolou) {
+        checarVencedor();
+    }
+}
+
+function resetarJogo() {
+    amigo1Rolou = false;
+    amigo2Rolou = false;
+    numerosAmigo1 = [];
+    numerosAmigo2 = [];
+    document.getElementById('botaoAmigo-1').setAttribute('enabled', true);
+    document.getElementById('botaoAmigo-2').setAttribute('enabled', true);
+}
+
+function rolarDado() {
+    return Math.round((Math.random() * 5) + 1); //Rola o dado para obter resultados entre 1 e 6
+}
+
+function checarVencedor() {
+    var somaAmigo1 = numerosAmigo1.reduce((n, v) => v + n);
+    var somaAmigo2 = numerosAmigo2.reduce((n, v) => v + n);
+
+    if (somaAmigo1 > somaAmigo2) {
+        alert("Amigo 1 é o vencedor!");
+    } else if (somaAmigo1 > somaAmigo2) {
+        alert("Amigo 2 é o vencedor!")
+    } else {
+        alert("Incrivelmente, um empate...")
+    }
+}
